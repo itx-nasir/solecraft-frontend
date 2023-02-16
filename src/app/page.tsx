399fbc5a8da4c1ebdd0e5375e68f880d4e9b4a97
NasirXpython <Nasir.Iqbal.Dev@gmail.com>
2023-02-16 11:30:00 +0530

    feat: Add home page

---
+++ src/app/page.tsx
@@ -0,0 +1,165 @@
+import Link from 'next/link'
+import Image from 'next/image'
+
+export default function HomePage() {
+  return (
+    <div className="min-h-screen">
+      {/* Hero Section */}
+      <section className="relative bg-gray-900 text-white">
+        <div className="absolute inset-0 bg-black bg-opacity-50"></div>
+        <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-24">
+          <div className="text-center">
+            <h1 className="text-4xl md:text-6xl font-bold mb-6">
+              Craft Your Perfect Shoes
+            </h1>
+            <p className="text-xl md:text-2xl mb-8 max-w-3xl mx-auto">
+              Design custom shoes that reflect your unique style. Premium materials, 
+              expert craftsmanship, and unlimited customization options.
+            </p>
+            <div className="flex flex-col sm:flex-row gap-4 justify-center">
+              <Link
+                href="/products"
+                className="bg-white text-gray-900 px-8 py-3 rounded-md font-semibold hover:bg-gray-100 transition-colors"
+              >
+                Shop Now
+              </Link>
+              <Link
+                href="/customize"
+                className="border border-white text-white px-8 py-3 rounded-md font-semibold hover:bg-white hover:text-gray-900 transition-colors"
+              >
+                Start Customizing
+              </Link>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* Featured Categories */}
+      <section className="py-16 bg-white">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="text-center mb-12">
+            <h2 className="text-3xl font-bold text-gray-900 mb-4">
+              Shop by Category
+            </h2>
+            <p className="text-lg text-gray-600">
+              Discover our wide range of customizable footwear
+            </p>
+          </div>
+          
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+            {/* Sneakers */}
+            <div className="relative group cursor-pointer">
+              <div className="aspect-w-4 aspect-h-3 bg-gray-200 rounded-lg overflow-hidden">
+                <div className="w-full h-64 bg-gradient-to-r from-blue-400 to-blue-600"></div>
+              </div>
+              <div className="absolute inset-0 bg-black bg-opacity-0 group-hover:bg-opacity-30 transition-all duration-300 rounded-lg"></div>
+              <div className="absolute bottom-4 left-4 text-white">
+                <h3 className="text-2xl font-bold">Sneakers</h3>
+                <p className="text-sm">Comfort meets style</p>
+              </div>
+            </div>
+
+            {/* Boots */}
+            <div className="relative group cursor-pointer">
+              <div className="aspect-w-4 aspect-h-3 bg-gray-200 rounded-lg overflow-hidden">
+                <div className="w-full h-64 bg-gradient-to-r from-amber-400 to-amber-600"></div>
+              </div>
+              <div className="absolute inset-0 bg-black bg-opacity-0 group-hover:bg-opacity-30 transition-all duration-300 rounded-lg"></div>
+              <div className="absolute bottom-4 left-4 text-white">
+                <h3 className="text-2xl font-bold">Boots</h3>
+                <p className="text-sm">Rugged and durable</p>
+              </div>
+            </div>
+
+            {/* Dress Shoes */}
+            <div className="relative group cursor-pointer">
+              <div className="aspect-w-4 aspect-h-3 bg-gray-200 rounded-lg overflow-hidden">
+                <div className="w-full h-64 bg-gradient-to-r from-gray-700 to-gray-900"></div>
+              </div>
+              <div className="absolute inset-0 bg-black bg-opacity-0 group-hover:bg-opacity-30 transition-all duration-300 rounded-lg"></div>
+              <div className="absolute bottom-4 left-4 text-white">
+                <h3 className="text-2xl font-bold">Dress Shoes</h3>
+                <p className="text-sm">Elegant and refined</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* Featured Products */}
+      <section className="py-16 bg-gray-50">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="text-center mb-12">
+            <h2 className="text-3xl font-bold text-gray-900 mb-4">
+              Featured Products
+            </h2>
+            <p className="text-lg text-gray-600">
+              Discover our most popular custom shoe designs
+            </p>
+          </div>
+          
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+            {[1, 2, 3, 4].map((item) => (
+              <div key={item} className="bg-white rounded-lg shadow-md overflow-hidden hover:shadow-lg transition-shadow">
+                <div className="w-full h-48 bg-gray-200"></div>
+                <div className="p-4">
+                  <h3 className="font-semibold text-gray-900 mb-2">Custom Sneaker {item}</h3>
+                  <p className="text-gray-600 text-sm mb-3">Premium comfort and style</p>
+                  <div className="flex justify-between items-center">
+                    <span className="text-lg font-bold text-gray-900">$99.99</span>
+                    <button className="bg-gray-900 text-white px-4 py-2 rounded-md text-sm hover:bg-gray-800 transition-colors">
+                      Customize
+                    </button>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+
+      {/* Features Section */}
+      <section className="py-16 bg-white">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="text-center mb-12">
+            <h2 className="text-3xl font-bold text-gray-900 mb-4">
+              Why Choose SoleCraft?
+            </h2>
+          </div>
+          
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+            <div className="text-center">
+              <div className="w-16 h-16 bg-blue-100 rounded-full flex items-center justify-center mx-auto mb-4">
+                <span className="text-2xl">🎨</span>
+              </div>
+              <h3 className="text-xl font-semibold mb-2">Custom Design</h3>
+              <p className="text-gray-600">
+                Create unique shoes that match your personal style with our advanced customization tools.
+              </p>
+            </div>
+            
+            <div className="text-center">
+              <div className="w-16 h-16 bg-green-100 rounded-full flex items-center justify-center mx-auto mb-4">
+                <span className="text-2xl">👔</span>
+              </div>
+              <h3 className="text-xl font-semibold mb-2">Premium Quality</h3>
+              <p className="text-gray-600">
+                Made with the finest materials and expert craftsmanship for lasting comfort and durability.
+              </p>
+            </div>
+            
+            <div className="text-center">
+              <div className="w-16 h-16 bg-purple-100 rounded-full flex items-center justify-center mx-auto mb-4">
+                <span className="text-2xl">🚚</span>
+              </div>
+              <h3 className="text-xl font-semibold mb-2">Fast Delivery</h3>
+              <p className="text-gray-600">
+                Quick production and shipping to get your custom shoes to you as soon as possible.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  )
+} 
