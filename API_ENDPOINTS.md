baea66dc3bb8d156f32aa62e9beca573821eb989
NasirXpython <Nasir.Iqbal.Dev@gmail.com>
2023-03-26 10:00:00 +0000

    feat: Add pages for admin

--- API_ENDPOINTS.md
+++ API_ENDPOINTS.md
@@ -152,7 +152,7 @@ Get current user's profile information.
     "is_guest": false,
     "is_active": true,
     "is_verified": true,
-    "is_staff": false,
+    "is_admin": false,
     "last_login": "2024-01-01T00:00:00Z",
     "created_at": "2024-01-01T00:00:00Z",
     "addresses": [],
@@ -244,7 +244,6 @@ Get paginated list of products with optional filters.
 **Query Parameters:**
 - `page` (int): Page number (default: 1)
 - `page_size` (int): Items per page (default: 20, max: 100)
-- `category_id` (UUID): Filter by category
 - `is_featured` (bool): Filter featured products
 
 **Response:**
@@ -346,7 +345,6 @@ Create a new product.
 {
   "name": "New Sneaker",
   "slug": "new-sneaker",
-  "category_id": "uuid",
   "base_price": 129.99,
   "description": "Product description",
   "short_description": "Brief description",
@@ -483,55 +481,6 @@ Get detailed order information.
 
 ---
 
-## 🏷️ Category Endpoints (To be implemented)
-
-### GET `/categories`
-Get all product categories with hierarchy.
-
-**Response:**
-```json
-{
-  "success": true,
-  "data": [
-    {
-      "id": "uuid",
-      "name": "Footwear",
-      "slug": "footwear",
-      "description": "All types of shoes",
-      "image_url": "category-image.jpg",
-      "is_active": true,
-      "sort_order": 1,
-      "children": [
-        {
-          "id": "uuid",
-          "name": "Sneakers",
-          "slug": "sneakers",
-          "parent_id": "uuid",
-          "children": []
-        }
-      ]
-    }
-  ]
-}
-```
-
-### GET `/categories/{category_id}`
-Get single category with products.
-
-### POST `/categories` 🔒
-Create new category.
-**Auth Required:** Yes (category:create permission)
-
-### PUT `/categories/{category_id}` 🔒
-Update category.
-**Auth Required:** Yes (category:update permission)
-
-### DELETE `/categories/{category_id}` 🔒
-Delete category.
-**Auth Required:** Yes (category:delete permission)
-
----
-
 ## 💳 Discount Endpoints (To be implemented)
 
 ### POST `/discounts/validate`
@@ -584,7 +533,6 @@ Search products with filters.
 
 **Query Parameters:**
 - `q` (string): Search query
-- `category_id` (UUID): Filter by category
 - `min_price` (decimal): Minimum price
 - `max_price` (decimal): Maximum price
 - `size` (string): Filter by size
@@ -673,11 +621,10 @@ Rate limit headers are included in responses:
 ## 📋 Notes for Frontend Development
 
 1. **Cart & Order endpoints** are defined in schemas but not yet implemented in API routes
-2. **Category endpoints** are defined in schemas but not yet implemented in API routes
-3. **Search functionality** is available in services but not exposed via API routes
-4. **File upload** for product images is configured but routes not implemented
-5. **Review system** is defined in schemas but API routes not implemented
-6. **Discount system** is defined in schemas but API routes not implemented
+2. **Search functionality** is available in services but not exposed via API routes
+3. **File upload** for product images is configured but routes not implemented
+4. **Review system** is defined in schemas but API routes not implemented
+5. **Discount system** is defined in schemas but API routes not implemented
 
 ## 🚀 Implementation Status
 
@@ -694,7 +641,6 @@ Rate limit headers are included in responses:
 ⏳ **To Be Implemented:**
 - Cart management endpoints
 - Order management endpoints
-- Category management endpoints
 - Search endpoints
 - Review endpoints
 - Discount/coupon endpoints
--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,189 +1,86 @@
-'use client'
+"use client";
 
-import { useState, useEffect } from 'react'
-import { useRouter } from 'next/navigation'
-import { Package, Users, ShoppingCart, TrendingUp, Settings } from 'lucide-react'
-import { useAuthStore } from '@/stores/authStore'
+import { useEffect, useState } from 'react';
+import { useAuthStore } from '@/stores/authStore';
+import { useRouter } from 'next/navigation';
+import { Users, ShoppingCart, TrendingUp, Package } from 'lucide-react';
+import { api } from '@/lib/api';
 
-export default function AdminPage() {
-  const { user, isAuthenticated } = useAuthStore()
-  const router = useRouter()
+interface DashboardStats {
+  total_users: number;
+  total_orders: number;
+  total_products: number;
+  revenue: number;
+}
+
+export default function AdminDashboardPage() {
+  const { user, isAuthenticated } = useAuthStore();
+  const router = useRouter();
+  const [stats, setStats] = useState<DashboardStats | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState('');
 
   useEffect(() => {
-    if (!isAuthenticated) {
-      router.push('/auth/login')
-    } else if (user && !user.is_staff) {
-      // Non-admin users shouldn't access this page
-      router.push('/')
+    if (!isAuthenticated || !user?.is_admin) {
+      router.push('/auth/login');
+      return;
     }
-  }, [isAuthenticated, user, router])
-
-  if (!isAuthenticated || !user || !user.is_staff) {
-    return (
-      <div className="min-h-screen flex items-center justify-center">
-        <div className="text-center">
-          <h2 className="text-2xl font-bold text-gray-900">Access Restricted</h2>
-          <p className="mt-2 text-gray-600">You need admin privileges to access this page.</p>
-        </div>
-      </div>
-    )
-  }
-
-  const adminStats = [
-    {
-      name: 'Total Products',
-      value: '150',
-      icon: Package,
-      change: '+12%',
-      changeType: 'increase'
-    },
-    {
-      name: 'Total Users',
-      value: '1,234',
-      icon: Users,
-      change: '+5%',
-      changeType: 'increase'
-    },
-    {
-      name: 'Total Orders',
-      value: '856',
-      icon: ShoppingCart,
-      change: '+8%',
-      changeType: 'increase'
-    },
-    {
-      name: 'Revenue',
-      value: '$12,345',
-      icon: TrendingUp,
-      change: '+15%',
-      changeType: 'increase'
-    }
-  ]
-
-  const quickActions = [
-    {
-      name: 'Manage Products',
-      description: 'Add, edit, or remove products',
-      icon: Package,
-      href: '/admin/products',
-      color: 'bg-blue-500'
-    },
-    {
-      name: 'Manage Users',
-      description: 'View and manage user accounts',
-      icon: Users,
-      href: '/admin/users',
-      color: 'bg-green-500'
-    },
-    {
-      name: 'View Orders',
-      description: 'Process and track orders',
-      icon: ShoppingCart,
-      href: '/admin/orders',
-      color: 'bg-yellow-500'
-    },
-    {
-      name: 'Settings',
-      description: 'System configuration',
-      icon: Settings,
-      href: '/admin/settings',
-      color: 'bg-purple-500'
-    }
-  ]
+    const fetchStats = async () => {
+      setLoading(true);
+      setError('');
+      try {
+        const response = await api.get('/admin/dashboard');
+        setStats(response.data);
+      } catch (err: any) {
+        setError('Failed to load dashboard stats.');
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchStats();
+  }, [isAuthenticated, user, router]);
 
   return (
-    <div className="min-h-screen bg-gray-50 py-12">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        {/* Header */}
-        <div className="mb-8">
-          <h1 className="text-3xl font-bold text-gray-900">Admin Dashboard</h1>
-          <p className="mt-2 text-gray-600">Welcome back, {user.first_name}! Here's your store overview.</p>
-        </div>
-
-        {/* Stats Grid */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mb-8">
-          {adminStats.map((stat) => (
-            <div key={stat.name} className="bg-white rounded-lg shadow p-6">
-              <div className="flex items-center">
-                <div className="bg-indigo-100 rounded-lg p-3">
-                  <stat.icon className="h-6 w-6 text-indigo-600" />
-                </div>
-                <div className="ml-4">
-                  <p className="text-sm font-medium text-gray-600">{stat.name}</p>
-                  <p className="text-2xl font-semibold text-gray-900">{stat.value}</p>
-                  <p className={`text-sm ${
-                    stat.changeType === 'increase' ? 'text-green-600' : 'text-red-600'
-                  }`}>
-                    {stat.change} from last month
-                  </p>
-                </div>
-              </div>
+    <div>
+      <h1 className="text-3xl font-bold text-gray-900 mb-8">Admin Dashboard</h1>
+      {loading ? (
+        <div className="text-center text-gray-500 py-12">Loading dashboard...</div>
+      ) : error ? (
+        <div className="text-center text-red-500 py-12">{error}</div>
+      ) : stats ? (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+          <div className="bg-white rounded-lg shadow p-6 flex items-center gap-4">
+            <Users className="h-10 w-10 text-blue-600" />
+            <div>
+              <div className="text-2xl font-bold text-gray-900">{stats.total_users}</div>
+              <div className="text-gray-600">Total Users</div>
             </div>
-          ))}
-        </div>
-
-        {/* Quick Actions */}
-        <div className="bg-white rounded-lg shadow">
-          <div className="px-6 py-4 border-b border-gray-200">
-            <h2 className="text-lg font-medium text-gray-900">Quick Actions</h2>
           </div>
-          <div className="p-6">
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
-              {quickActions.map((action) => (
-                <button
-                  key={action.name}
-                  onClick={() => alert(`${action.name} coming soon!`)}
-                  className="text-left p-6 border border-gray-200 rounded-lg hover:shadow-md transition-shadow"
-                >
-                  <div className={`${action.color} rounded-lg p-3 w-fit mb-4`}>
-                    <action.icon className="h-6 w-6 text-white" />
-                  </div>
-                  <h3 className="font-medium text-gray-900 mb-2">{action.name}</h3>
-                  <p className="text-sm text-gray-600">{action.description}</p>
-                </button>
-              ))}
+          <div className="bg-white rounded-lg shadow p-6 flex items-center gap-4">
+            <ShoppingCart className="h-10 w-10 text-green-600" />
+            <div>
+              <div className="text-2xl font-bold text-gray-900">{stats.total_orders}</div>
+              <div className="text-gray-600">Total Orders</div>
+            </div>
+          </div>
+          <div className="bg-white rounded-lg shadow p-6 flex items-center gap-4">
+            <Package className="h-10 w-10 text-purple-600" />
+            <div>
+              <div className="text-2xl font-bold text-gray-900">{stats.total_products}</div>
+              <div className="text-gray-600">Total Products</div>
+            </div>
+          </div>
+          <div className="bg-white rounded-lg shadow p-6 flex items-center gap-4">
+            <TrendingUp className="h-10 w-10 text-yellow-600" />
+            <div>
+              <div className="text-2xl font-bold text-gray-900">
+                ${typeof stats.revenue === 'number' ? stats.revenue.toLocaleString() : '0'}
+              </div>
+              <div className="text-gray-600">Revenue</div>
             </div>
           </div>
         </div>
-
-        {/* Recent Activity */}
-        <div className="bg-white rounded-lg shadow mt-8">
-          <div className="px-6 py-4 border-b border-gray-200">
-            <h2 className="text-lg font-medium text-gray-900">Recent Activity</h2>
-          </div>
-          <div className="p-6">
-            <div className="space-y-4">
-              <div className="flex items-center space-x-4">
-                <div className="bg-green-100 rounded-full p-2">
-                  <ShoppingCart className="h-4 w-4 text-green-600" />
-                </div>
-                <div>
-                  <p className="text-sm font-medium text-gray-900">New order #ORD-001234</p>
-                  <p className="text-sm text-gray-600">2 minutes ago</p>
-                </div>
-              </div>
-              <div className="flex items-center space-x-4">
-                <div className="bg-blue-100 rounded-full p-2">
-                  <Users className="h-4 w-4 text-blue-600" />
-                </div>
-                <div>
-                  <p className="text-sm font-medium text-gray-900">New user registration</p>
-                  <p className="text-sm text-gray-600">5 minutes ago</p>
-                </div>
-              </div>
-              <div className="flex items-center space-x-4">
-                <div className="bg-yellow-100 rounded-full p-2">
-                  <Package className="h-4 w-4 text-yellow-600" />
-                </div>
-                <div>
-                  <p className="text-sm font-medium text-gray-900">Product inventory low</p>
-                  <p className="text-sm text-gray-600">1 hour ago</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      ) : null}
     </div>
-  )
+  );
 } 
